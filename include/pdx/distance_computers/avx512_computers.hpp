7eec1ce203d4286f20f1d681348baf8ba1d406c9
lkuffo <lxkr@cwi.nl>
2025-06-16 14:32:51 +0200

    lep8 escape code with avx512

--- include/pdx/distance_computers/avx512_computers.hpp
+++ include/pdx/distance_computers/avx512_computers.hpp
@@ -819,8 +819,6 @@ public:
             //     std::cout << exc_offset_0 << " exceptions were skipped [0].\n";
             // }
             if constexpr (!SKIP_PRUNED) {
-                assert(n_exceptions == exc_offset_0);
-                assert(n_exceptions == exc_offset_1);
                 if (n_exceptions != exc_offset_0) {
                     std::cout << n_exceptions << " exceptions present [0].\n";
                     std::cout << exc_offset_0 << " exceptions were skipped [0].\n";
@@ -829,6 +827,8 @@ public:
                     std::cout << n_exceptions << " exceptions present [1].\n";
                     std::cout << exc_offset_0 << " exceptions were skipped [1].\n";
                 }
+                assert(n_exceptions == exc_offset_0);
+                assert(n_exceptions == exc_offset_1);
             }
         }
     }
