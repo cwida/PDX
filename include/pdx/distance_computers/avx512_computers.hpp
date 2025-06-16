0cc260f78aa7a5521fa05d92d89a0bb848a856ed
lkuffo <lxkr@cwi.nl>
2025-06-16 10:37:18 +0200

    lep8 escape code with avx512

--- include/pdx/distance_computers/avx512_computers.hpp
+++ include/pdx/distance_computers/avx512_computers.hpp
@@ -754,6 +754,7 @@ public:
                 else {
                     float diff_high = exceptions_query[dim_idx] - (float)(exceptions_data[exc_start_0 + exc_offset_0]);
                     distances_p[vector_idx] += diff_high * diff_high * scaling_factors_exceptions[dim_idx];
+                    exc_offset_0 += 1;
                 }
                 if (nibble_1 != EXC_ESCAPE_CODE_SCALAR) {
                     float diff_low = query_dim_1 - (float)(nibble_1);
@@ -762,6 +763,7 @@ public:
                 else {
                     float diff_high = exceptions_query[dim_idx + 1] - (float)(exceptions_data[exc_start_1 + exc_offset_1]);
                     distances_p[vector_idx] += diff_high * diff_high * scaling_factors_exceptions[dim_idx + 1];
+                    exc_offset_1 += 1;
                 }
 
                 // float to_multiply_a = query_dim_0 - (float)nibble_0; // High
