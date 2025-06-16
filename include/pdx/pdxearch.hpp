eca7042768c0abbb00d84474a8e98272e9ae5af4
lkuffo <lxkr@cwi.nl>
2025-06-16 13:37:49 +0200

    lep8 escape code with avx512

--- include/pdx/pdxearch.hpp
+++ include/pdx/pdxearch.hpp
@@ -482,7 +482,7 @@ protected:
         uint32_t n_tuples_to_prune = 0;
         if (!is_positional_pruning) GetPruningThreshold(k, heap);
         while (
-                //1.0 * n_tuples_to_prune < tuples_needed_to_exit &&
+                1.0 * n_tuples_to_prune < tuples_needed_to_exit &&
                 current_dimension_idx < pdx_data.num_vertical_dimensions) {
             size_t last_dimension_to_fetch = std::min(current_dimension_idx + DIMENSIONS_FETCHING_SIZES[cur_subgrouping_size_idx],
                                                       pdx_data.num_vertical_dimensions);
